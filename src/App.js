fad294fce147de762bcb613d2c445f087a06969b
digallardox <digallardox@gmail.com>
2021-12-15 17:19:04 -0600

    basic mobile-responsive navbar with routing to a home and login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import { Helmet } from 'react-helmet';
+import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navigationbar from './components/Navbar';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
     <div className="App">
       <Helmet title="Playing For Keeps"/>
+
+      <Navigationbar />
+
+      <Routes>
+        <Route path='/login' element={<LoginForm />}/>
+      </Routes>
+
     </div>
   );
 }
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,9 @@
+import { button } from 'react-bootstrap';
+
+export default function LoginForm(){
+    return (
+        <>
+        <h2>Login</h2>
+        </>
+    )
+}
